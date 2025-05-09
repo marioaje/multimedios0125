44f7b4cd0b4ddd73ccbec8421c0b2d56bf07e2cd
marioaje <marioaje@gmail.com>
2025-05-09 08:36:49 -0600

    Update index.php

--- Semana08/index.php
+++ Semana08/index.php
@@ -9,6 +9,54 @@
  $estado = true;
  $decimales = 1.23;
 
+
+
+
+ $t = date("H");
+ if ($t < "20") {
+    echo "Have a good day!";
+ } else {
+    echo "Have a good night!";
+ }
+
+echo ($t < "20") ? "Have a good day!" :  "Have a good night!";
+
+
+
+
+
+
+
+
+
+
+
+
+ //Para definir una constante
+ define("URL_API",'https://paginas-web-cr.com/Api/apis/');
+ const consultarAPI ='ListaCurso.php';
+
+//palabra_reservada  nombrefuncion (posiblesparametros)
+function saludos(){
+    echo "Saludos clase<br>";
+}
+
+function sumar($dato1, $dato2){
+    return $dato1 + $dato2;
+}
+
+saludos();
+$resultados = sumar(12, 6);
+echo $resultados."<br>";
+
+
+
+echo URL_API;
+echo "<br>";
+echo consultarAPI;
+echo "<br>";
+
+
 //Ciclos
 for ($i=0; $i < 10; $i++) { 
     echo "Numero: $i <br> ";
