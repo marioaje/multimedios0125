a4f67ba08fe68dd96e38252dd39d3c25526bb68c
marioaje <marioaje@gmail.com>
2025-05-28 09:08:24 -0600

    Update UsuarioController.php

--- Proyecto php/controller/UsuarioController.php
+++ Proyecto php/controller/UsuarioController.php
@@ -20,14 +20,29 @@
             return $this->dao->obtenerPorId($id);
         }
 
-     public function insertar(Usuario $objeto){
-            return $this->dao->insertar(objeto);
+        public function insertar(Usuario $objeto){
+            return $this->dao->insertar($objeto);
         }
 
     }
 
     if ($_SERVER['REQUEST_METHOD'] == "POST"){
-        echo "Trabajando";
+
+            $nombre = $_POST['nombre'];
+            $apellidos = $_POST['apellidos'];
+            $estado = $_POST['estado'];
+        
+            $objeto = new Usuario(null, $nombre, $apellidos, $estado);
+
+            $controlador = new UsuarioController();
+            $controlador->insertar($objeto);
+            
+            header("Location: ../view/Usuario");
+            exit();
+        // capturan los parametros, los incluyen en un objeto
+        // --->me invoquen la funcion insertar
+        
+
     }
 
 ?>
