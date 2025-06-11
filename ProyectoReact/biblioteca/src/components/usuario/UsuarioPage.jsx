7b822b39c5371e79428f1c45b15908883cadbb83
marioaje <marioaje@gmail.com>
2025-06-11 10:38:20 -0600

    Carga de datos en lista

--- ProyectoReact/biblioteca/src/components/usuario/UsuarioPage.jsx
+++ ProyectoReact/biblioteca/src/components/usuario/UsuarioPage.jsx
@@ -1,9 +1,61 @@
 //C:\xampp\htdocs\2025\multimedios0125\ProyectoReact\biblioteca\src\components\usuario
 //src\components\usuario
+import { obtenerUsuario } from '../../services/usuarioService';
+import { useEffect, useState } from 'react';
 const UsuarioPage = () => {
+
+    //Set y get analogia
+    const [ usuarios, setUsuarios ] = useState([]); //variables de estado 
+    const [ cargando, setCargando ] = useState(true);
+
+
+    //useEffect es una anologia del documentReady
+    useEffect( ()=>
+        {
+        obtenerUsuario()
+                .then((respuesta) =>{
+                    console.log(respuesta.data.data);
+                    setUsuarios(respuesta.data.data);
+                }
+            )
+            .catch((error) => {
+                    console.error('Error', error);
+                }
+            )
+        }, []       
+    );
+    
+
+
+
     return(
         <div>
             <h2>Listado de Usuarios</h2>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th> id </th>
+                        <th> name </th>
+                        <th> email </th>
+                        <th> acciones </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        usuarios.map((items) => 
+                              (
+                                <tr key={items.id}>
+                                    <td>{items.id}</td>
+                                    <td>{items.name}</td>
+                                    <td>{items.email}</td>
+                                    <td>Editar || Eliminar </td>
+                                </tr>
+                            )
+                        )
+                    }
+                </tbody>
+            </table>
         </div>
     );
 };
